a10f05305ba0c7ae6c1aea435f672dff95d35eb9
Arminnn-droid <arminmalekm44@gmail.com>
2022-11-28 16:21:28 +0330

    well, trying to write some file!

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner feelScanner = new Scanner(System.in);
-        System.out.println("Well, how are you today?!");
-        String feelings = feelScanner.nextLine();
-        System.out.println("Well, you're: " + feelings);
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("file.txt"));
+            writer.write("hello world!!" +
+                    "\nHow are you doing?!");
+            writer.close();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 }
