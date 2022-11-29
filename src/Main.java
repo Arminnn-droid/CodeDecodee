56456f86ada6b7b34e8a9b56a1efd5c4e21daf02
Arminnn-droid <arminmalekm44@gmail.com>
2022-11-29 18:54:35 +0330

    well, Ready for send

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-//ArminMalekMohammadiBidhendi     39912330117022    The encrypt decrypt practice
+/*
+
+                   ArminMalekMohammadiBidhendi     39912330117022    The encrypt decrypt practice
+
+ */
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
-        String fileDir = "C:\\Users\\armin\\CodeDecode\\file.txt";
+        String fileDir = "";// آدرس فایل خود را در اینجا قرار دهید!
         encryptDecrypt newFile = new encryptDecrypt(fileDir);
 
         try {
