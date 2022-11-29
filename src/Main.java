db088190d0052799399555a8e54943c7e7f25428
Arminnn-droid <arminmalekm44@gmail.com>
2022-11-29 18:45:49 +0330

    well, successful try to write the program!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
-import java.io.*;
-import java.util.Scanner;
-//import java.util.Scanner;
+//ArminMalekMohammadiBidhendi     39912330117022    The encrypt decrypt practice
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
+        String fileDir = "C:\\Users\\armin\\CodeDecode\\file.txt";
+        encryptDecrypt newFile = new encryptDecrypt(fileDir);
+
+        try {
+            newFile.code();
+            newFile.code();
+            newFile.decode();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/encryptDecrypt.java
+++ src/encryptDecrypt.java
@@ -1,10 +1,82 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class encryptDecrypt {
 
     String fileDirectory;
-    public void encryptDecrypt(String fileDirectory_){
+    boolean isEncrypted;
+    public encryptDecrypt(String fileDirectory_){
         fileDirectory = fileDirectory_;
+    }
+    public void changeFileURL(String newFileDirectory){ fileDirectory = newFileDirectory; }
+    public void code() throws IOException {
+
+        if ( !isEncrypted ){
+
+            File userFile = new File(fileDirectory);
+            Scanner fileScan = new Scanner(userFile);
+            String fileContext = "";
+            String codedFile = "";
+
+            while (fileScan.hasNextLine()){ fileContext = fileContext.concat((fileScan.nextLine()+"\n")); }
+
+            char[] charArray = fileContext.toCharArray();
+
+            for(int x = 0; x < fileContext.length(); x++){
+                int id = (int) charArray[x];
+                int codeId = 2*id +2;
+                charArray[x] = (char) codeId;
+            }
+
+            for(int x = 0; x < charArray.length; x++){
+                codedFile += charArray[x];
+            }
+
+            FileWriter codedWriter = new FileWriter(fileDirectory);
+            codedWriter.flush();
+            codedWriter.write(codedFile);
+            codedWriter.close();
+
+            isEncrypted = true;
+
+        }else System.out.println("The file is already encrypted!");
+
     }
 
 
+    public void decode() throws IOException {
 
+        if ( isEncrypted == true ){
+
+            File userFile = new File(fileDirectory);
+            Scanner fileScan = new Scanner(userFile);
+            String codedFIle = "";
+            String decodedFile = "";
+
+            while (fileScan.hasNextLine()){ codedFIle = codedFIle.concat((fileScan.nextLine()+"\n")); }
+
+            char[] charArray = codedFIle.toCharArray();
+
+            for(int x = 0; x < codedFIle.length(); x++){
+                int id = (int) charArray[x];
+                int codeId = id/2 -1;
+                charArray[x] = (char) codeId;
+            }
+
+            for(int x = 0; x < charArray.length; x++){
+                decodedFile += charArray[x];
+            }
+
+            FileWriter codedWriter = new FileWriter(fileDirectory);
+            codedWriter.flush();
+            codedWriter.write(decodedFile);
+            codedWriter.close();
+
+            isEncrypted = true;
+
+        }else System.out.println("The file is not encrypted or it is not encrypted by our formula!");
+
+    }
 }
